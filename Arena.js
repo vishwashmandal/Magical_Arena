b5e891b9a9e257b80b422c99bad3b1146eebce06
vishwashmandal <vishwashmandal53@gmail.com>
2024-05-05 13:45:27 +0530

    Under PlayMatch() function add final logic when player2 health less than player1 match

--- Arena.js
+++ Arena.js
@@ -68,7 +68,42 @@ class Arena {
            console.log("Player 1 Wins!") 
 
         }
+        //Second Player start the game
+        else
+        {
+            //In this case i take flag is odd number because second player health is less than first player health  second player start the game
+            let flag =3;
+            while (this.player1.health > 0 && this.player2.health > 0) {
+                let attackValue, defendValue, damage;
+                
+                if (flag % 2 == 0) {
+                    attackValue = this.player1.attackRoll();
+                    defendValue = this.player2.defendRoll();
+                    damage = this.player1.attack * attackValue - this.player2.strength * defendValue;
+                    // Ensuring damage is non-negative if damage is negative its means plyer defend successfully without healh loss
+                    if (damage < 0) damage = 0; 
+                    this.player2.takeDamage(damage);
+                }
+                else 
+                 {
+                    attackValue = this.player2.attackRoll();
+                    defendValue = this.player1.defendRoll();
+                    damage = this.player2.attack * attackValue - this.player1.strength * defendValue;
+                    // Ensuring damage is non-negative
+                    if (damage < 0) damage = 0; 
+                    this.player1.takeDamage(damage);
+                }
+    
+    
+                flag++;
+            }
+            if(this.player1.health<=0)
+            console.log("Player 2 Wins!")
+           else
+           console.log("Player 1 Wins!") 
+
         
+        }
         
 
         
