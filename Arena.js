d85a53916ff48064678a7b401e6fde59f5c62d0e
vishwashmandal <vishwashmandal53@gmail.com>
2024-05-05 13:42:30 +0530

    Define the Arena Class under this class under this a function PlayMatch() under this function initialize first condition when first player health is less than secind pplayer health so first player start game first

--- Arena.js
+++ Arena.js
@@ -23,6 +23,60 @@ class Player {
     }
 }
 
+class Arena {
+    //Constructor to intialize playesr in the arena
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+//Method to simulate a match between players and arena
+    playMatch() {
+        if(this.player1.health < this.player2.health)
+        {
+            
+            //Ensure that every time play diffent player example if flag%2==0 than first player play
+            // if not than second player play and last i increase flag by value 1
+            let flag =2;
+            while (this.player1.health > 0 && this.player2.health > 0) {
+                let attackValue, defendValue, damage;
+                
+                if (flag % 2 == 0) {
+                    attackValue = this.player1.attackRoll();
+                    defendValue = this.player2.defendRoll();
+                    damage = this.player1.attack * attackValue - this.player2.strength * defendValue;
+                    // Ensuring damage is non-negative if damage is negative its means plyer defend successfully without healh loss
+                    if (damage < 0) damage=0;
+                    this.player2.takeDamage(damage);
+                }
+                else 
+                 {
+                    attackValue = this.player2.attackRoll();
+                    defendValue = this.player1.defendRoll();
+                    damage = this.player2.attack * attackValue - this.player1.strength * defendValue;
+                    // Ensuring damage is non-negative if damage is negative its means plyer defend successfully without healh loss
+                    if (damage < 0) damage = 0; 
+                    this.player1.takeDamage(damage);
+                }
+    
+    
+                flag++;
+            }
+            //By Rules if first player health is 0 than second player wins and vice versa
+            if(this.player1.health<=0)
+            console.log("Player 2 Wins!")
+           else
+           console.log("Player 1 Wins!") 
+
+        }
+        
+        
+
+        
+    }
+
+}
+
+
 
 
 
